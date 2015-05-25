01109638459e3bf8498e597670d850e1fbd8f8d7
ardnech <ardnech@gmail.com>
2015-05-25 16:49:59 +0200

    office end hour

--- ForecastAndAnalysing/fDataForecasting.Designer.cs
+++ ForecastAndAnalysing/fDataForecasting.Designer.cs
@@ -35,6 +35,8 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox_dataForecasting_productList = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
@@ -45,6 +47,7 @@
             this.button_AcceptationSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button_AcceptationSend.Location = new System.Drawing.Point(1038, 3);
             this.button_AcceptationSend.Name = "button_AcceptationSend";
+            this.button_AcceptationSend.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.button_AcceptationSend.Size = new System.Drawing.Size(154, 38);
             this.button_AcceptationSend.TabIndex = 0;
             this.button_AcceptationSend.Text = "Send to Acceptation";
@@ -54,48 +57,75 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
+            this.tableLayoutPanel1.SetColumnSpan(this.chart1, 2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(3, 72);
+            this.chart1.Location = new System.Drawing.Point(3, 143);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1189, 232);
+            this.chart1.Size = new System.Drawing.Size(1189, 220);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 2);
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 375);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 419);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1189, 339);
+            this.dataGridView1.Size = new System.Drawing.Size(1189, 295);
             this.dataGridView1.TabIndex = 2;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1019F));
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button_AcceptationSend, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Controls.Add(this.comboBox_dataForecasting_productList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button_AcceptationSend, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 3);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 238F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 129F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.85714F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.14286F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1195, 738);
             this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // comboBox_dataForecasting_productList
+            // 
+            this.comboBox_dataForecasting_productList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox_dataForecasting_productList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox_dataForecasting_productList.FormattingEnabled = true;
+            this.comboBox_dataForecasting_productList.Location = new System.Drawing.Point(3, 3);
+            this.comboBox_dataForecasting_productList.Name = "comboBox_dataForecasting_productList";
+            this.comboBox_dataForecasting_productList.Size = new System.Drawing.Size(170, 123);
+            this.comboBox_dataForecasting_productList.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 369);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // fDataForecasting
             // 
@@ -105,9 +135,11 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "fDataForecasting";
             this.Text = "dataForecasting";
+            this.Load += new System.EventHandler(this.fDataForecasting_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -118,5 +150,7 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox_dataForecasting_productList;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- ForecastAndAnalysing/fDataForecasting.cs
+++ ForecastAndAnalysing/fDataForecasting.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace ForecastAndAnalysing
 {
@@ -15,6 +16,49 @@ namespace ForecastAndAnalysing
         public fDataForecasting()
         {
             InitializeComponent();
+        }
+
+        private void fDataForecasting_Load(object sender, EventArgs e)
+        {
+            databaseConnectivity dbConn = new databaseConnectivity();
+
+
+            DataTable dtProductList = new DataTable();
+            dbConn.getSqlData("common.getProductList", dtProductList);
+
+            comboBox_dataForecasting_productList.DataSource = dtProductList;
+            comboBox_dataForecasting_productList.DisplayMember = "productName";
+            comboBox_dataForecasting_productList.ValueMember = "id";
+
+            DataTable dtProductValues = new DataTable();
+            dbConn.getSqlData("forecast.productData 1", dtProductValues);
+
+            chart1.DataSource = dtProductValues;
+            chart1.Series.First().XValueMember = "month";
+            chart1.Series.First().YValueMembers = "value";
+            chart1.DataBind();
+
+            chart1.Series.Add("TrendLine");
+            chart1.Series["TrendLine"].ChartType = SeriesChartType.Line;
+            chart1.Series["TrendLine"].BorderWidth = 1;
+            chart1.Series["TrendLine"].Color = Color.Red;
+            // Line of best fit is linear
+            string typeRegression = "Linear";//"Exponential";//
+                                             // The number of days for Forecasting
+            string forecasting = "1";
+            // Show Error as a range chart.
+            string error = "false";
+            // Show Forecasting Error as a range chart.
+            string forecastingError = "false";
+            // Formula parameters
+            string parameters = typeRegression + ',' + forecasting + ',' + error + ',' + forecastingError;
+            chart1.Series[0].Sort(PointSortOrder.Ascending, "X");
+            // Create Forecasting Series.
+            chart1.DataManipulator.FinancialFormula(FinancialFormula.Forecasting, parameters, chart1.Series[0], chart1.Series["TrendLine"]);
+            //chart1.DataManipulator.FinancialFormula()
+            chart1.Update();
+
+            textBox1.Text = comboBox_dataForecasting_productList.SelectedValue.ToString();
         }
     }
 }
